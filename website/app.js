77378dc909ed4befc51e4fc0d1e1112eaee0f243
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-10-12 07:29:31 +0200

    posting fetched data and user data to my server

--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 
+// const { userInfo } = require("os");
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
@@ -12,12 +14,41 @@ const getWeather = async (url, zipCode, key) => {
   const res = await fetch(url + zipCode + key);
   try {
     const data = await res.json();
-    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const postWeather = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
   } catch (error) {
     console.log("error", error);
   }
 };
 
 document.querySelector("#generate").addEventListener("click", () => {
-  getWeather(baseUrl, 90011, apiKey);
+  let zip = document.querySelector("#zip").value;
+  let userResponse = document.querySelector("#feelings").value;
+  getWeather(baseUrl, zip, apiKey).then((data) => {
+    console.log(data);
+    let weatherObj = {
+      temperature: data.main.temp,
+      date: newDate,
+      userResponse,
+    };
+    postWeather("/add", weatherObj);
+  });
 });
